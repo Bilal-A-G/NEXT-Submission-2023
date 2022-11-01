47a554109ac23704680d87a1602b29e63f15764d
unknown <bilalabdulg@gmail.com>
2022-11-01 10:46:26 -0400

    Created a math library and added a rotating cube

---
+++ Software 3D Renderer/src/Math.h
@@ -0,0 +1,164 @@
+﻿#pragma once
+
+namespace TESLA
+{
+    struct Matrix4x4;
+    
+    struct Vector
+    {
+        float x,y,z,w;
+    public:
+        Vector(float x, float y, float z, float w):
+        x(x), y(y), z(z), w(w){}
+        
+        Vector(float x, float y, float z):
+        x(x), y(y), z(z), w(1){}
+
+        Vector(){}
+
+        Vector operator *(const Vector& other) const
+        {
+            return {other.x * x, other.y * y, other.z * z, other.w * w};
+        }
+
+        Vector operator *(const float& other) const
+        {
+            return  {x * other, y * other, z * other, w};
+        }
+
+        //Defined later on because matrix's body isn't here yet
+        Vector operator *(const Matrix4x4& b) const;
+    };
+
+    struct Matrix4x4
+    {
+        Vector r1, r2, r3, r4;
+    public:
+        Matrix4x4(Vector r1, Vector r2, Vector r3, Vector r4):
+        r1(r1), r2(r2), r3(r3), r4(r4){}
+
+        Matrix4x4(){}
+
+        Matrix4x4 operator *(const Matrix4x4& b) const
+        {
+            Matrix4x4 returnMatrix;
+
+            returnMatrix.r1 = Vector(
+                (r1.x * b.r1.x + r1.y * b.r2.x + r1.z * b.r3.x + r1.w * b.r4.x),
+                (r1.x * b.r1.y + r1.y * b.r2.y + r1.z * b.r3.y + r1.w * b.r4.y),
+                (r1.x * b.r1.z + r1.y * b.r2.z + r1.z * b.r3.z + r1.w * b.r4.z),
+                (r1.x * b.r1.w + r1.y * b.r2.w + r1.z * b.r3.w + r1.w * b.r4.w)
+            );
+
+            returnMatrix.r2 = Vector(
+                (r2.x * b.r1.x + r2.y * b.r2.x + r2.z * b.r3.x + r2.w * b.r4.x),
+                (r2.x * b.r1.y + r2.y * b.r2.y + r2.z * b.r3.y + r2.w * b.r4.y),
+                (r2.x * b.r1.z + r2.y * b.r2.z + r2.z * b.r3.z + r2.w * b.r4.z),
+                (r2.x * b.r1.w + r2.y * b.r2.w + r2.z * b.r3.w + r2.w * b.r4.w)
+            );
+
+            returnMatrix.r3 = Vector(
+                (r3.x * b.r1.x + r3.y * b.r2.x + r3.z * b.r3.x + r3.w * b.r4.x),
+                (r3.x * b.r1.y + r3.y * b.r2.y + r3.z * b.r3.y + r3.w * b.r4.y),
+                (r3.x * b.r1.z + r3.y * b.r2.z + r3.z * b.r3.z + r3.w * b.r4.z),
+                (r3.x * b.r1.w + r3.y * b.r2.w + r3.z * b.r3.w + r3.w * b.r4.w)
+            );
+            returnMatrix.r4 = Vector(
+                (r4.x * b.r1.x + r4.y * b.r2.x + r4.z * b.r3.x + r4.w * b.r4.x),
+                (r4.x * b.r1.y + r4.y * b.r2.y + r4.z * b.r3.y + r4.w * b.r4.y),
+                (r4.x * b.r1.z + r4.y * b.r2.z + r4.z * b.r3.z + r4.w * b.r4.z),
+                (r4.x * b.r1.w + r4.y * b.r2.w + r4.z * b.r3.w + r4.w * b.r4.w)
+            );
+        
+            return returnMatrix;
+        }
+
+        Matrix4x4 operator+(const Matrix4x4& b) const
+        {
+            Matrix4x4 returnMatrix;
+
+            returnMatrix.r1 = Vector(
+                (r1.x + b.r1.x),
+                (r1.y + b.r1.y),
+                (r1.z + b.r1.z),
+                (r1.w + b.r1.w)
+            );
+
+            returnMatrix.r2 = Vector(
+                (r2.x + b.r2.x),
+                (r2.y + b.r2.y),
+                (r2.z + b.r2.z),
+                (r2.w + b.r2.w)
+            );
+
+            returnMatrix.r3 = Vector(
+                (r3.x + b.r3.x),
+                (r3.y + b.r3.y),
+                (r3.z + b.r3.z),
+                (r3.w + b.r3.w)
+            );
+
+            returnMatrix.r4 = Vector(
+                (r4.x + b.r4.x),
+                (r4.y + b.r4.y),
+                (r4.z + b.r4.z),
+                (r4.w + b.r4.w)
+            );
+        
+            return returnMatrix;
+        }
+    };
+
+    inline Vector Vector::operator*(const Matrix4x4& b) const
+    {
+        Vector returnVector;
+
+        returnVector.x = x * b.r1.x + y * b.r2.x + z * b.r3.x + b.r4.x;
+        returnVector.y = x * b.r1.y + y * b.r2.y + z * b.r3.y + b.r4.y;
+        returnVector.z = x * b.r1.z + y * b.r2.z + z * b.r3.z + b.r4.z;
+        returnVector.w = x * b.r1.w + y * b.r2.w + z * b.r3.w + b.r4.w;
+        
+        return returnVector;
+    }
+
+
+    class Triangle
+    {
+    public:
+        std::array<Vector, 3> vertices;
+    public:
+        Triangle(std::initializer_list<Vector> vertices):vertices()
+        {
+            this->vertices[0] = data(vertices)[0];
+            this->vertices[1] = data(vertices)[1];
+            this->vertices[2] = data(vertices)[2];
+        }
+
+        Triangle(){}
+    };
+
+    struct Mesh
+    {
+        std::vector<Triangle> triangles;
+    public:
+        Mesh(std::vector<Triangle> triangles):
+        triangles(triangles){}
+
+        Mesh(){}
+    };
+
+    static Vector PerspectiveDivide(Vector a)
+    {
+        Vector returnVector{a.x, a.y, a.z};
+        
+        if(a.w != 0)
+        {
+            returnVector.x/=a.w;
+            returnVector.y/=a.w;
+            returnVector.z/=a.w;
+        }
+
+        return returnVector;
+    }
+    
+}
--- Software 3D Renderer/src/main.cpp
+++ Software 3D Renderer/src/main.cpp
@@ -1,20 +1,79 @@
+#include <array>
 #include "NextAPI/app.h"
+#include "Math.h"
+
+TESLA::Mesh* cube;
+TESLA::Matrix4x4* projection;
+constexpr float cubeSize = 2;
+
+constexpr float nearPlane = 0.1f;
+constexpr float farPlane = 1000.0f;
+constexpr float fov = 90.0f;
+constexpr float aspectRatio = GLUT_SCREEN_HEIGHT/GLUT_SCREEN_WIDTH;
+const float fovRad = 1.0f / tan(fov/2 / PI * 180);
 
 //------------------------------------------------------------------------
 // Called before first update. Do any initial setup here.
 //------------------------------------------------------------------------
 void Init()
 {
+	TESLA::Vector bottomLeft = TESLA::Vector(-1, -1, -1) * cubeSize * 100;
+	TESLA::Vector bottomRight = TESLA::Vector(1, -1, -1) * cubeSize * 100;
 
+	TESLA::Vector topLeft = TESLA::Vector(-1, 1, -1) * cubeSize * 100;
+	TESLA::Vector topRight = TESLA::Vector(1, 1, -1) * cubeSize * 100;
+
+	TESLA::Vector bottomLeftBack = TESLA::Vector(-1, -1, 1) * cubeSize * 100;
+	TESLA::Vector bottomRightBack = TESLA::Vector(1, -1, 1) * cubeSize * 100;
+
+	TESLA::Vector topLeftBack = TESLA::Vector(-1, 1, 1) * cubeSize * 100;
+	TESLA::Vector topRightBack = TESLA::Vector(1, 1, 1) * cubeSize * 100;
+	
+	const std::vector<TESLA::Triangle> triangles
+	{
+		//Front
+		{bottomLeft, topLeft, topRight},
+		{topRight, bottomRight, bottomLeft},
+
+		//Back
+		{ bottomLeftBack, topLeftBack, topRightBack},
+		{topRightBack, bottomRightBack, bottomLeftBack},
+
+		//Left
+		{topLeftBack, topLeft, bottomLeft},
+		{bottomLeft, bottomLeftBack, topLeftBack},
+
+		//Right
+		{topRight, topRightBack, bottomRightBack},
+		{bottomRightBack, bottomRight, topRight},
+
+		//Up
+		{topLeftBack, topRightBack, topRight},
+		{topRight, topLeft, topLeftBack},
+
+		//Down
+		{bottomRightBack, bottomLeftBack, bottomLeft},
+		{bottomLeft, bottomRight, bottomRightBack},
+	};
+
+	projection = new TESLA::Matrix4x4{
+		{aspectRatio * fovRad, 0.0f, 0.f, 0.0f},
+		{0.0f, fovRad, 0.0f, 0.0f},
+		{0.0f, 0.0f, farPlane/(farPlane - nearPlane), 1.0f},
+		{0.0f, 0.0f, -farPlane * nearPlane/(farPlane - nearPlane), 0.0f}
+	};
+
+	cube = new TESLA::Mesh(triangles);
 }
 
 //------------------------------------------------------------------------
 // Update your simulation here. deltaTime is the elapsed time since the last update in ms.
 // This will be called at no greater frequency than the value of APP_MAX_FRAME_RATE
 //------------------------------------------------------------------------
+float accumulatedTime;
 void Update(float deltaTime)
 {
-	
+	accumulatedTime += 1 * deltaTime/1000;
 }
 
 //------------------------------------------------------------------------
@@ -22,10 +81,60 @@ void Update(float deltaTime)
 // See App.h 
 //------------------------------------------------------------------------
 void Render()
-{	
-	App::Print(100, 100, "Sample Text");
+{
+	float sinTheta = static_cast<float>(sin(accumulatedTime));
+	float cosTheta = static_cast<float>(cos(accumulatedTime));
+	
+	TESLA::Matrix4x4 rotationY
+	{
+		{cosTheta, 0.0f, -sinTheta, 0.0f},
+		{0.0f, 1.0f, 0.0f, 0.0f},
+		{sinTheta, 0.0f, cosTheta, 0.0f},
+		{0.0f, 0.0f, 0.0f, 1.0f}
+	};
 
-	App::DrawLine(150, 150, 200, 200);
+	TESLA::Matrix4x4 rotationX
+	{
+		{1.0f, 0.0f, 0.0f, 0.0f},
+		{0.0f, cosTheta, sinTheta, 0.0f},
+		{0.0f, -sinTheta, cosTheta, 0.0f},
+		{0.0f, 0.0f, 0.0f, 1.0f}
+	};
+
+	TESLA::Matrix4x4 rotationZ
+	{
+		{cosTheta, sinTheta, 0.0f, 0.0f},
+		{-sinTheta, cosTheta, 0.0f, 0.0f},
+		{0.0f, 0.0f, 1.0f, 0.0f},
+		{0.0f, 0.0f, 0.0f, 1.0f}
+	};
+	
+	for (TESLA::Triangle triangle : cube->triangles)
+	{
+		TESLA::Triangle triangleProjected;
+
+		triangleProjected.vertices[0] = triangle.vertices[0] * rotationX * rotationY * rotationZ * *projection;
+		triangleProjected.vertices[1] =	triangle.vertices[1] * rotationX * rotationY * rotationZ * *projection;
+		triangleProjected.vertices[2] =	triangle.vertices[2] * rotationX * rotationY * rotationZ * *projection;
+
+		triangleProjected.vertices[0].x += 500.0f;
+		triangleProjected.vertices[0].y += 250.0f;
+		triangleProjected.vertices[1].x += 500.0f;
+		triangleProjected.vertices[1].y += 250.0f;
+		triangleProjected.vertices[2].x += 500.0f;
+		triangleProjected.vertices[2].y += 250.0f;
+	
+		App::DrawLine(triangleProjected.vertices[0].x, triangleProjected.vertices[0].y,
+			triangleProjected.vertices[1].x, triangleProjected.vertices[1].y);
+	
+		App::DrawLine(triangleProjected.vertices[1].x, triangleProjected.vertices[1].y,
+			triangleProjected.vertices[2].x, triangleProjected.vertices[2].y);
+	
+		// App::DrawLine(triangleProjected.vertices[2].x, triangleProjected.vertices[2].y,
+		// 	triangleProjected.vertices[0].x, triangleProjected.vertices[0].y);
+	}
+	
+	App::Print(100, 100, "Amogus");
 }
 
 //------------------------------------------------------------------------
@@ -34,5 +143,6 @@ void Render()
 //------------------------------------------------------------------------
 void Shutdown()
 {	
-
+	delete(cube);
+	delete(projection);
 }
